ca232946be0e03fb37ae73e6bc595fe5311170f8
Nastassia-Mir <nmironovic909@gmail.com>
2023-12-03 12:48:57 +0300

    61,62,63

--- freecodecamp-8.js
+++
@@ -1,9 +0,0 @@
-// Variable declarations
-var StUdLyCapVaR;
-var properCamelCase;
-var TitleCaseOver;
-
-// Variable assignments
-STUDLYCAPVAR = 10;
-PRoperCAmelCAse = "A String";
-tITLEcASEoVER = 9000;
